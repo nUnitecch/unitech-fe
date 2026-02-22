9b57b2639bcae012f76cd3d4450a2f4ac3de9d6b
Cleve-keem <belloharyourmidey@gmail.com>
2026-02-22 09:13:39 -0600

    chore: responsively displayed Feature card container

--- components/Home/CardSection.tsx
+++ components/Home/CardSection.tsx
@@ -5,7 +5,7 @@ import Container from "../Container";
 export default function CardSection() {
   return (
     <Container>
-      <div className="px-4 pt-20 grid grid-cols-1 gap-5">
+      <div className="px-4 pt-20 grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {FEATURE_CARDS.map((card, id) => (
           <FeatureCard details={card} key={id} />
         ))}
